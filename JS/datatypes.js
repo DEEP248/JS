0cf8e182527db1e28c438b19cb9f4bf9513f9f26
Deep Darji <96133840+DEEP248@users.noreply.github.com>
2024-03-05 17:51:35 +0000

    datatypes basics

---
+++ JS/datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all js code to new version.
+//alert("Hello"); // Using Node js Not Browser Not Working
+let name="deep"
+let age = 18
+let isLoggedin = true
+
+console.log(typeof age);
+console.log(typeof "deep");
+console.log(typeof undefined); //output will be undefined
+console.log(typeof null); //output will be Object
